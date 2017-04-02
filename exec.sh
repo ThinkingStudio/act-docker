85fb2076212c242a9b743434f8402344620baca8
Joe Cincotta <joe@pixolut.com>
2017-04-02 16:41:58 +1000

    this is a WIP. Copied from my test dir. Is not working yet...

---
+++ exec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir actframework
+cd actframework
+git clone https://github.com/actframework/act-demo-apps.git
+cd act-demo-apps
+cd helloworld
+mvn clean package
+cd target/dist
+unzip *
+chmod 755 ./start
+./start
